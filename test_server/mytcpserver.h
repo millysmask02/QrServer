b753019130033f1c826cbd4a7762d399ee5af7d0
Zentor66 <155506765+Zentor66@users.noreply.github.com>
2024-12-20 14:23:41 +0300

    Add files via upload

---
+++ test_server/mytcpserver.h
@@ -0,0 +1,29 @@
+#ifndef MYTCPSERVER_H
+#define MYTCPSERVER_H
+
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+class MyTcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyTcpServer(QObject *parent = 0);
+
+public slots:
+    void slotNewConnection();
+    void slotServerRead();
+
+private:
+    enum class EnumTypeRequest {
+        Form,
+        Approved,
+        Error,
+    };
+    QTcpServer * mTcpServer;
+    QTcpSocket * mTcpSocket;
+    EnumTypeRequest rowsChecking(QString row);
+};
+
+#endif // MYTCPSERVER_H
